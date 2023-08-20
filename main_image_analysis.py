ee4f77b570631e675d3796c682e2ef9cb917aa60
Ruby Hartono <58564005+rh-id@users.noreply.github.com>
2023-08-20 21:25:28 +0800

    [analyze_image] add color_map and color_space

--- main_image_analysis.py
+++ main_image_analysis.py
@@ -6,13 +6,18 @@ from scripts import image_utils
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument("image_path", help="Path to the image file")
-    parser.add_argument("analyze_type", choices=['smooth', 'sharpen', 'color_split'],
+    parser.add_argument("analyze_type", choices=['smooth', 'sharpen',
+                                                 'color_map', 'color_space', 'color_split'],
                         help="Type of image analysis that you want to perform")
     args = parser.parse_args()
-    # TODO color_space,filter2d_kernels,morph,skin_segment
+    # TODO filter2d_kernels,morph,skin_segment
     if args.analyze_type == 'smooth':
         image_utils.analyze_image_smooth(args.image_path)
     elif args.analyze_type == 'sharpen':
         image_utils.analyze_image_sharpen(args.image_path)
+    elif args.analyze_type == 'color_map':
+        image_utils.analyze_image_color_map(args.image_path)
     elif args.analyze_type == 'color_split':
         image_utils.analyze_image_color_split(args.image_path)
+    elif args.analyze_type == 'color_space':
+        image_utils.analyze_image_color_space(args.image_path)
--- scripts/image_utils.py
+++ scripts/image_utils.py
@@ -3,9 +3,94 @@ import numpy as np
 from matplotlib import pyplot as plt
 
 
+def analyze_image_color_map(image_path):
+    def show_with_matplotlib(color_img, title, pos):
+        img_RGB = color_img[:, :, ::-1]
+
+        plt.subplot(4, 7, pos)
+        plt.imshow(img_RGB)
+        plt.title(title)
+        plt.axis('off')
+
+    gray_img = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
+
+    colormaps = ['AUTUMN', 'BONE', 'JET', 'WINTER', 'RAINBOW', 'OCEAN',
+                 'SUMMER', 'SPRING', 'COOL', 'HSV', 'PINK', 'HOT',
+                 'PARULA', 'MAGMA', 'INFERNO', 'PLASMA', 'VIRIDIS', 'CIVIDIS',
+                 'TWILIGHT', 'TWILIGHT_SHIFTED', 'TURBO', 'DEEPGREEN']
+
+    plt.figure(figsize=(12, 5))
+    plt.suptitle("Colormaps", fontsize=14, fontweight='bold')
+
+    show_with_matplotlib(cv2.cvtColor(gray_img, cv2.COLOR_GRAY2BGR), "GRAY", 1)
+
+    for idx, val in enumerate(colormaps):
+        show_with_matplotlib(cv2.applyColorMap(gray_img, idx), val, idx + 2)
+
+    plt.show()
+
+
+# Analyze image with color map
+def analyze_image_color_space(image_path):
+    def show_with_matplotlib(color_img, title, pos):
+        img_RGB = color_img[:, :, ::-1]
+
+        plt.subplot(3, 6, pos)
+        plt.imshow(img_RGB)
+        plt.title(title)
+        plt.axis('off')
+
+    image = cv2.imread(image_path)
+
+    plt.figure(figsize=(12, 5))
+    plt.suptitle("Color spaces in OpenCV", fontsize=14, fontweight='bold')
+
+    gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+    (bgr_b, bgr_g, bgr_r) = cv2.split(image)
+
+    hsv_image = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+    (hsv_h, hsv_s, hsv_v) = cv2.split(hsv_image)
+
+    hls_image = cv2.cvtColor(image, cv2.COLOR_BGR2HLS)
+    (hls_h, hls_l, hls_s) = cv2.split(hls_image)
+
+    ycrcb_image = cv2.cvtColor(image, cv2.COLOR_BGR2YCrCb)
+    (ycrcb_y, ycrcb_cr, ycrcb_cb) = cv2.split(ycrcb_image)
+
+    lab_image = cv2.cvtColor(image, cv2.COLOR_BGR2LAB)
+    (lab_l, lab_a, lab_b) = cv2.split(lab_image)
+
+    show_with_matplotlib(image, "BGR - image", 1)
+
+    show_with_matplotlib(cv2.cvtColor(gray_image, cv2.COLOR_GRAY2BGR), "gray image", 1 + 6)
+
+    show_with_matplotlib(cv2.cvtColor(bgr_b, cv2.COLOR_GRAY2BGR), "BGR - B comp", 2)
+    show_with_matplotlib(cv2.cvtColor(bgr_g, cv2.COLOR_GRAY2BGR), "BGR - G comp", 2 + 6)
+    show_with_matplotlib(cv2.cvtColor(bgr_r, cv2.COLOR_GRAY2BGR), "BGR - R comp", 2 + 6 * 2)
+
+    show_with_matplotlib(cv2.cvtColor(hsv_h, cv2.COLOR_GRAY2BGR), "HSV - H comp", 3)
+    show_with_matplotlib(cv2.cvtColor(hsv_s, cv2.COLOR_GRAY2BGR), "HSV - S comp", 3 + 6)
+    show_with_matplotlib(cv2.cvtColor(hsv_v, cv2.COLOR_GRAY2BGR), "HSV - V comp", 3 + 6 * 2)
+
+    show_with_matplotlib(cv2.cvtColor(hls_h, cv2.COLOR_GRAY2BGR), "HLS - H comp", 4)
+    show_with_matplotlib(cv2.cvtColor(hls_l, cv2.COLOR_GRAY2BGR), "HLS - L comp", 4 + 6)
+    show_with_matplotlib(cv2.cvtColor(hls_s, cv2.COLOR_GRAY2BGR), "HLS - S comp", 4 + 6 * 2)
+
+    show_with_matplotlib(cv2.cvtColor(ycrcb_y, cv2.COLOR_GRAY2BGR), "YCrCb - Y comp", 5)
+    show_with_matplotlib(cv2.cvtColor(ycrcb_cr, cv2.COLOR_GRAY2BGR), "YCrCb - Cr comp", 5 + 6)
+    show_with_matplotlib(cv2.cvtColor(ycrcb_cb, cv2.COLOR_GRAY2BGR), "YCrCb - Cb comp", 5 + 6 * 2)
+
+    show_with_matplotlib(cv2.cvtColor(lab_l, cv2.COLOR_GRAY2BGR), "L*a*b - L comp", 6)
+    show_with_matplotlib(cv2.cvtColor(lab_a, cv2.COLOR_GRAY2BGR), "L*a*b - a comp", 6 + 6)
+    show_with_matplotlib(cv2.cvtColor(lab_b, cv2.COLOR_GRAY2BGR), "L*a*b - b comp", 6 + 6 * 2)
+
+    plt.show()
+
+
+# Analyze image with color split
 def analyze_image_color_split(image_path):
     def show_with_matplotlib(color_img, title, pos):
-        # Convert BGR image to RGB
         img_RGB = color_img[:, :, ::-1]
 
         plt.subplot(3, 6, pos)
@@ -65,7 +150,6 @@ def analyze_image_color_split(image_path):
 # Analyze image with multiple sharpen tech
 def analyze_image_sharpen(image_path):
     def show_with_matplotlib(color_img, title, pos):
-        # Convert BGR image to RGB
         img_RGB = color_img[:, :, ::-1]
 
         plt.subplot(2, 3, pos)
@@ -127,7 +211,6 @@ def analyze_image_sharpen(image_path):
 # Analyze image with multiple smooth tech
 def analyze_image_smooth(image_path):
     def show_with_matplotlib(color_img, title, pos):
-        # Convert BGR image to RGB
         img_RGB = color_img[:, :, ::-1]
 
         plt.subplot(3, 3, pos)
