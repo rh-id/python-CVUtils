4c99f54cb6a366bd983ec484f36ea4d88c782b2f
Ruby Hartono <58564005+rh-id@users.noreply.github.com>
2023-08-21 14:00:01 +0800

    [analyze_image] add kernel

--- main_image_analysis.py
+++ main_image_analysis.py
@@ -6,13 +6,14 @@ from scripts import image_utils
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument("image_path", help="Path to the image file")
-    parser.add_argument("analyze_type", choices=['morph', 'sharpen', 'skin_segment',
+    parser.add_argument("analyze_type", choices=['kernel', 'morph', 'sharpen', 'skin_segment',
                                                  'smooth', 'color_map', 'color_space',
                                                  'color_split'],
                         help="Type of image analysis that you want to perform")
     args = parser.parse_args()
-    # TODO filter2d_kernels
-    if args.analyze_type == 'morph':
+    if args.analyze_type == 'kernel':
+        image_utils.analyze_image_kernel(args.image_path)
+    elif args.analyze_type == 'morph':
         image_utils.analyze_image_morph(args.image_path)
     elif args.analyze_type == 'sharpen':
         image_utils.analyze_image_sharpen(args.image_path)
--- scripts/image_utils.py
+++ scripts/image_utils.py
@@ -3,6 +3,101 @@ import numpy as np
 from matplotlib import pyplot as plt
 
 
+def analyze_image_kernel(image_path):
+    def show_with_matplotlib(color_img, title, pos):
+        img_RGB = color_img[:, :, ::-1]
+
+        plt.subplot(3, 4, pos)
+        plt.imshow(img_RGB)
+        plt.title(title)
+        plt.axis('off')
+
+    plt.figure(figsize=(12, 6))
+    plt.suptitle("Comparing different kernels using cv2.filter2D()", fontsize=14, fontweight='bold')
+
+    image = cv2.imread(image_path)
+
+    kernel_identity = np.array([[0, 0, 0],
+                                [0, 1, 0],
+                                [0, 0, 0]])
+
+    kernel_edge_detection_1 = np.array([[1, 0, -1],
+                                        [0, 0, 0],
+                                        [-1, 0, 1]])
+
+    kernel_edge_detection_2 = np.array([[0, 1, 0],
+                                        [1, -4, 1],
+                                        [0, 1, 0]])
+
+    kernel_edge_detection_3 = np.array([[-1, -1, -1],
+                                        [-1, 8, -1],
+                                        [-1, -1, -1]])
+
+    kernel_sharpen = np.array([[0, -1, 0],
+                               [-1, 5, -1],
+                               [0, -1, 0]])
+
+    kernel_unsharp_masking = -1 / 256 * np.array([[1, 4, 6, 4, 1],
+                                                  [4, 16, 24, 16, 4],
+                                                  [6, 24, -476, 24, 6],
+                                                  [4, 16, 24, 16, 4],
+                                                  [1, 4, 6, 4, 1]])
+
+    kernel_blur = 1 / 9 * np.array([[1, 1, 1],
+                                    [1, 1, 1],
+                                    [1, 1, 1]])
+
+    gaussian_blur = 1 / 16 * np.array([[1, 2, 1],
+                                       [2, 4, 2],
+                                       [1, 2, 1]])
+
+    kernel_emboss = np.array([[-2, -1, 0],
+                              [-1, 1, 1],
+                              [0, 1, 2]])
+
+    sobel_x_kernel = np.array([[1, 0, -1],
+                               [2, 0, -2],
+                               [1, 0, -1]])
+
+    sobel_y_kernel = np.array([[1, 2, 1],
+                               [0, 0, 0],
+                               [-1, -2, -1]])
+
+    outline_kernel = np.array([[-1, -1, -1],
+                               [-1, 8, -1],
+                               [-1, -1, -1]])
+
+    original_image = cv2.filter2D(image, -1, kernel_identity)
+    edge_image_1 = cv2.filter2D(image, -1, kernel_edge_detection_1)
+    edge_image_2 = cv2.filter2D(image, -1, kernel_edge_detection_2)
+    edge_image_3 = cv2.filter2D(image, -1, kernel_edge_detection_3)
+    sharpen_image = cv2.filter2D(image, -1, kernel_sharpen)
+    unsharp_masking_image = cv2.filter2D(image, -1, kernel_unsharp_masking)
+    blur_image = cv2.filter2D(image, -1, kernel_blur)
+    gaussian_blur_image = cv2.filter2D(image, -1, gaussian_blur)
+    emboss_image = cv2.filter2D(image, -1, kernel_emboss)
+    sobel_x_image = cv2.filter2D(image, -1, sobel_x_kernel)
+    sobel_y_image = cv2.filter2D(image, -1, sobel_y_kernel)
+    outline_image = cv2.filter2D(image, -1, outline_kernel)
+
+    # Show all the images:
+    show_with_matplotlib(original_image, "identity kernel", 1)
+    show_with_matplotlib(edge_image_1, "edge detection 1", 2)
+    show_with_matplotlib(edge_image_2, "edge detection 2", 3)
+    show_with_matplotlib(edge_image_3, "edge detection 3", 4)
+    show_with_matplotlib(sharpen_image, "sharpen", 5)
+    show_with_matplotlib(unsharp_masking_image, "unsharp masking", 6)
+    show_with_matplotlib(blur_image, "blur image", 7)
+    show_with_matplotlib(gaussian_blur_image, "gaussian blur image", 8)
+    show_with_matplotlib(emboss_image, "emboss image", 9)
+    show_with_matplotlib(sobel_x_image, "sobel x image", 10)
+    show_with_matplotlib(sobel_y_image, "sobel y image", 11)
+    show_with_matplotlib(outline_image, "outline image", 12)
+
+    # Show the Figure:
+    plt.show()
+
+
 def analyze_image_skin_segment(image_path):
     def show_with_matplotlib(color_img, title, pos):
         img_RGB = color_img[:, :, ::-1]
