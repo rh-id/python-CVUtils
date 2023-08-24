59313a6658db946cdf511d973ebc74ae3055deb3
Ruby Hartono <58564005+rh-id@users.noreply.github.com>
2023-08-24 19:39:40 +0700

    [image_threshold] add adaptive_filter_noise

--- main_image_threshold.py
+++ main_image_threshold.py
@@ -5,10 +5,13 @@ from scripts import image_utils
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument("image_path", help="Path to the image file")
-    parser.add_argument("threshold_type", choices=['binary', 'adaptive'],
+    parser.add_argument("threshold_type", choices=['adaptive', 'adaptive_filter_noise',
+                                                   'binary', ],
                         help="Threshold type that you want to perform")
     args = parser.parse_args()
-    if args.threshold_type == 'binary':
-        image_utils.threshold_image_binary(args.image_path)
-    elif args.threshold_type == 'adaptive':
+    if args.threshold_type == 'adaptive':
         image_utils.threshold_image_adaptive(args.image_path)
+    elif args.threshold_type == 'adaptive_filter_noise':
+        image_utils.threshold_image_adaptive_filter_noise(args.image_path)
+    elif args.threshold_type == 'binary':
+        image_utils.threshold_image_binary(args.image_path)
--- scripts/image_utils.py
+++ scripts/image_utils.py
@@ -3,6 +3,39 @@ import numpy as np
 from matplotlib import pyplot as plt
 
 
+def threshold_image_adaptive_filter_noise(image_path):
+    def show_img_with_matplotlib(color_img, title, pos):
+        img_RGB = color_img[:, :, ::-1]
+
+        plt.subplot(2, 3, pos)
+        plt.imshow(img_RGB)
+        plt.title(title)
+        plt.axis('off')
+
+    fig = plt.figure(figsize=(15, 7))
+    plt.suptitle("Thresholding Adaptive + Bilateral Filter (noise removal while edges sharp)", fontsize=14,
+                 fontweight='bold')
+    fig.patch.set_facecolor('silver')
+
+    image = cv2.imread(image_path)
+    gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+    gray_image = cv2.bilateralFilter(gray_image, 15, 25, 25)
+
+    thresh1 = cv2.adaptiveThreshold(gray_image, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 11, 2)
+    thresh2 = cv2.adaptiveThreshold(gray_image, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 31, 3)
+    thresh3 = cv2.adaptiveThreshold(gray_image, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 11, 2)
+    thresh4 = cv2.adaptiveThreshold(gray_image, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 31, 3)
+
+    show_img_with_matplotlib(cv2.cvtColor(gray_image, cv2.COLOR_GRAY2BGR), "gray img", 1)
+    show_img_with_matplotlib(cv2.cvtColor(thresh1, cv2.COLOR_GRAY2BGR), "THRESH_MEAN_C, blockSize=11, C=2", 2)
+    show_img_with_matplotlib(cv2.cvtColor(thresh2, cv2.COLOR_GRAY2BGR), "THRESH_MEAN_C, blockSize=31, C=3", 3)
+    show_img_with_matplotlib(cv2.cvtColor(thresh3, cv2.COLOR_GRAY2BGR), "GAUSSIAN_C, blockSize=11, C=2", 5)
+    show_img_with_matplotlib(cv2.cvtColor(thresh4, cv2.COLOR_GRAY2BGR), "GAUSSIAN_C, blockSize=31, C=3", 6)
+
+    plt.show()
+
+
 def threshold_image_adaptive(image_path):
     def show_img_with_matplotlib(color_img, title, pos):
         img_RGB = color_img[:, :, ::-1]
