3ba9a3f1f6ac8497dcc0379379019ad8e62d0199
Ruby Hartono <58564005+rh-id@users.noreply.github.com>
2023-08-24 19:57:22 +0700

    [image_threshold] add bgr

--- main_image_threshold.py
+++ main_image_threshold.py
@@ -6,12 +6,14 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument("image_path", help="Path to the image file")
     parser.add_argument("threshold_type", choices=['adaptive', 'adaptive_filter_noise',
-                                                   'binary', ],
+                                                   'bgr', 'binary', ],
                         help="Threshold type that you want to perform")
     args = parser.parse_args()
     if args.threshold_type == 'adaptive':
         image_utils.threshold_image_adaptive(args.image_path)
     elif args.threshold_type == 'adaptive_filter_noise':
         image_utils.threshold_image_adaptive_filter_noise(args.image_path)
+    elif args.threshold_type == 'bgr':
+        image_utils.threshold_image_bgr(args.image_path)
     elif args.threshold_type == 'binary':
         image_utils.threshold_image_binary(args.image_path)
--- scripts/image_utils.py
+++ scripts/image_utils.py
@@ -3,6 +3,37 @@ import numpy as np
 from matplotlib import pyplot as plt
 
 
+def threshold_image_bgr(image_path):
+    def show_img_with_matplotlib(color_img, title, pos):
+        img_RGB = color_img[:, :, ::-1]
+
+        plt.subplot(1, 3, pos)
+        plt.imshow(img_RGB)
+        plt.title(title)
+        plt.axis('off')
+
+    fig = plt.figure(figsize=(12, 4))
+    plt.suptitle("Thresholding BGR", fontsize=14, fontweight='bold')
+    fig.patch.set_facecolor('silver')
+
+    image = cv2.imread(image_path)
+
+    show_img_with_matplotlib(image, "image", 1)
+
+    ret1, thresh1 = cv2.threshold(image, 120, 255, cv2.THRESH_BINARY)
+
+    (b, g, r) = cv2.split(image)
+    ret2, thresh2 = cv2.threshold(b, 120, 255, cv2.THRESH_BINARY)
+    ret3, thresh3 = cv2.threshold(g, 120, 255, cv2.THRESH_BINARY)
+    ret4, thresh4 = cv2.threshold(r, 120, 255, cv2.THRESH_BINARY)
+    bgr_thresh = cv2.merge((thresh2, thresh3, thresh4))
+
+    show_img_with_matplotlib(thresh1, "threshold (120) BGR", 2)
+    show_img_with_matplotlib(bgr_thresh, "threshold (120) each channel and merge", 3)
+
+    plt.show()
+
+
 def threshold_image_adaptive_filter_noise(image_path):
     def show_img_with_matplotlib(color_img, title, pos):
         img_RGB = color_img[:, :, ::-1]
